c82677f6eda11e09e91d706f0bec4add6bce7e3e
Do Tran Nam Anh <namanh14122005@gmail.com>
2025-01-20 14:04:18 +0700

    map chưa load được dòng 2

--- Main/Tile/TileManager.java
+++ Main/Tile/TileManager.java
@@ -1,18 +1,27 @@
 package Tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javax.imageio.ImageIO;
 import Game.GamePanel;
 
 public class TileManager {
     GamePanel gp;
     Tile[] tile;
-
+    int mapTileNum[][];
+    
+    
     public TileManager(GamePanel gp) {
         this.gp = gp;
         tile = new Tile[10];
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+   
         getTileImage();
+        loadMap();
     }
 
     public void getTileImage() {
@@ -30,12 +39,48 @@ public class TileManager {
         }
     }
     
+    public void loadMap() {
+    	try {
+    		InputStream is = getClass().getResourceAsStream("/map/map01.txt");
+    		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    		
+    		int col = 0, row = 0;
+    		
+    		while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+    			
+    			String line = br.readLine(); // READ LINE FROM THE TEXT FILE
+    			
+    			while(col < gp.maxScreenCol) {
+    				
+    				String numbers[] = line.split(" "); // chia cách các space trong 0 1 0 0 1
+    				
+    				int num = Integer.parseInt(numbers[col]); // chuyển từ string qua int
+    				
+    				mapTileNum[col][row] = num;
+    				col++;
+    			}
+    			if(col == gp.maxScreenCol) {
+    				col = 0; 
+    				row += 1;				
+    			}
+    			br.close(); // close 
+    		}
+    	}catch(Exception e) {
+    		
+    	}
+    }
+    
+    
     public void draw(Graphics2D g2) {
     	
     	int col = 0; int row = 0; int x = 0, y= 0;
     	
     	while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
-    		g2.drawImage(tile[0].image, x, y, gp.titleSize,gp.titleSize,null);
+    		
+    		
+    		int tileNum = mapTileNum[col][row]; // dữ liệu map đã được lưu vào array
+    		
+    		g2.drawImage(tile[tileNum].image, x, y, gp.titleSize,gp.titleSize,null);
     		col++; 
     		x += gp.titleSize;
     		
